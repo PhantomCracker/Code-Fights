e868e1cf9ffd46ff71afc75228f928bccc5ff2fd
Popa Vlad <popa.vlad94@gmail.com>
2024-01-22 20:26:16 +0200

    update for maximum points

--- Code-Arcade/The-Core/Mirror Lake/isSubstitutionCypher.js
+++ Code-Arcade/The-Core/Mirror Lake/isSubstitutionCypher.js
@@ -1,25 +1,35 @@
 function solution(string1, string2) {
-    let occurancesInString1 = occurancesInString(string1).sort((a, b) => b - a);
-    let occurancesInString2 = occurancesInString(string2).sort((a, b) => b - a);
-
-    if (checkIfArraysAreEqual(occurancesInString1, occurancesInString2)) {
-        return true;
-    }
-    return false;
-}
-
-function occurancesInString(s) {
-    let occurances = new Array(26).fill(0);
-
-    for (let char of s) {
-        occurances[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    if (string1.length !== string2.length) {
+        return false;
     }
 
-    return occurances;
-}
+    const mappings = {};
 
-function checkIfArraysAreEqual(a1, a2) {
-    return  Array.isArray(a1) && Array.isArray(a2) &&
-        a1.length === a2.length &&
-        a1.every((val, index) => val === a2[index])
+    for (let i = 0; i < string1.length; i++) {
+        const char1 = string1[i];
+        const char2 = string2[i];
+
+        if (!mappings.hasOwnProperty(char1)) {
+            mappings[char1] = char2;
+        } else {
+            if (mappings[char1] !== char2) {
+                return false;
+            }
+
+            const charOccurrenceCount1 = string1.split(char1).length - 1;
+            const charOccurrenceCount2 = string2.split(char2).length - 1;
+
+            if (charOccurrenceCount1 !== charOccurrenceCount2) {
+                return false;
+            }
+        }
+    }
+
+    for (const [char, mappedChar] of Object.entries(mappings)) {
+        if (string2.indexOf(mappedChar) !== string1.indexOf(char)) {
+            return false;
+        }
+    }
+
+    return true;
 }
